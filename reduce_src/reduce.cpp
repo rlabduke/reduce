fa3836ee72309c559ce27e7fecb4787bd25e52d3
barendall <barendall@4f46053f-4816-0410-b275-d6d3daee6e87>
2013-02-19 21:46:33 +0000

    approaching release of a multi-change update version: with changes to H bond-length parameters; addition of option to use nuclear distances rather than electron cloud distances; overhaul of alternate conformation handling; addition of ability to consider aromatic methyl rotation; and removal of N-terminal residue N-H.  So, with all this, need to ensure that date and version number will be meaningful in future.  reduce version # 3.21 reduce date 130219 associated reduce revision # 172 before this check-in # 173 after.
    git-svn-id: https://quiddity.biochem.duke.edu/svn/reduce/trunk@173 4f46053f-4816-0410-b275-d6d3daee6e87

--- reduce_src/reduce.cpp
+++ reduce_src/reduce.cpp
@@ -24,9 +24,9 @@
 #endif
 
 static const char *versionString =
-     "reduce: version 3.21 01/22/2013, Copyright 1997-2013, J. Michael Word";
+     "reduce: version 3.21 02/19/2013, Copyright 1997-2013, J. Michael Word";
 
-static const char *shortVersion    = "reduce.3.21.130122";
+static const char *shortVersion    = "reduce.3.21.130219";
 static const char *referenceString =
                        "Word, et. al. (1999) J. Mol. Biol. 285, 1735-1747.";
 static const char *electronicReference = "http://kinemage.biochem.duke.edu";
@@ -1002,6 +1002,7 @@ void reduceChanges(bool showAll) { /*changes*/
    cerr  << "2013/01/16 - jjh       Added -NUCLEAR flag, which uses nuclear distances/vdW rather" << endl;
    cerr  << "                         the default electron cloud distances/vdW for H placement" << endl;
    cerr  << "2013/01/22 - jjh       fixed handling of group rotation for alternates" << endl;
+   cerr  << "2013/02/19 - wba       updated version number and date" << endl;
    cerr  << endl;
    exit(1);
 }
