758db3e08c8457985d73105987f67b733ad63b71
jheadd <jheadd@4f46053f-4816-0410-b275-d6d3daee6e87>
2013-12-13 15:05:58 +0000

    fixed bug in the handling of single character residue names e.g. 'X  ' to correctly right-justify these; updated wwPDB_het_dict to reflect corrected formatting and to include new hets
    git-svn-id: https://quiddity.biochem.duke.edu/svn/reduce/trunk@190 4f46053f-4816-0410-b275-d6d3daee6e87

--- update_het_dict.bash
+++ update_het_dict.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 ######################################
 #Gary Kapral 04/20/2011
-#update the het dictionary in reduce on the cluster; downloads the het dictionary from PDB, date stamps it 
+#update the het dictionary in reduce on the cluster; downloads the het dictionary from PDB, date stamps it
 #makes it readable by reduce, and links the reduce_wwPDB_het_dict.txt used by reduce to the new het dictionary
 #use: sign in as srcer, run from /home/srcer/src/reduce/reduce_trunk/
 #other programs used: adjust_het_dict.pl, comment_out_OH_on_P_v3.0.pl
@@ -12,6 +12,6 @@ curl --output reduce_wwPDB_het_dict$DATESTAMP.unmod.txt --stderr curlerr.log ftp
 perl ./adjust_het_dict.pl reduce_wwPDB_het_dict$DATESTAMP.unmod.txt reduce_het_dict$DATESTAMP.adjusted.txt					#get rid of unnecessary spaces
 perl ./comment_out_OH_on_P_v3.0.pl reduce_het_dict$DATESTAMP.adjusted.txt									#comment out the H on phosphate that PDB puts in for neutral charge
 mv reduce_het_dict$DATESTAMP.adjusted.txt_mod reduce_wwPDB_het_dict$DATESTAMP.txt								#rename the new file follow the standard name format
-rm /home/srcer/bin/reduce_wwPDB_het_dict.txt 													#remove the old symbolic link
-ln -s /home/srcer/src/reduce/reduce_trunk/reduce_wwPDB_het_dict$DATESTAMP.txt /home/srcer/bin/reduce_wwPDB_het_dict.txt				#add a new symbolic link that points to the latest het_dict
+#rm /home/srcer/bin/reduce_wwPDB_het_dict.txt 													#remove the old symbolic link
+#ln -s /home/srcer/src/reduce/reduce_trunk/reduce_wwPDB_het_dict$DATESTAMP.txt /home/srcer/bin/reduce_wwPDB_het_dict.txt				#add a new symbolic link that points to the latest het_dict
 
