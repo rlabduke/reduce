rlabduke/reduce

commit f5b3046be9c20d82be2974bdf457a9c68d6a808e

More standard-compliant use of vector reserve() vs. resize() and push_back() vs. []

reduce_src/reduce.cpp
@@ -1887,7 +1887,7 @@ void genHydrogens(const atomPlacementPlan& pp, ResBlk& theRes, bool o2prime,
 				std::list<PDBrec*> rs;
 				theRes.get(pp.conn(i), rs);
 				if (!rs.empty()) {
-					nconf[i] = rs.size();
+					nconf.push_back(rs.size());
 					maxalt = std::max(maxalt, nconf[i]);
 					std::vector<PDBrec*> rvv_v;
 					rvv_v.reserve(nconf[i]);

reduce_src/AtomPositions.cpp
@@ -871,13 +871,13 @@ int AtomPositions::SearchClique(std::list<MoverPtr> clique, int limit)
 	const int numItems = clique.size();
 	std::vector<MoverPtr>    item (numItems);
 	std::vector<double> currScore;
-	currScore.reserve(numItems);
+	currScore.resize(numItems);
 	std::vector<float>   currBump;
-	currBump.reserve(numItems);
+	currBump.resize(numItems);
 	std::vector<float>  currHbond;
-	currHbond.reserve(numItems);
+	currHbond.resize(numItems);
 	std::vector<bool> currBadBump;
-	currBadBump.reserve(numItems);
+	currBadBump.resize(numItems);
 	std::vector<std::list<AtomDescr> >   atomsInCliq( numItems );
 	std::vector<std::list<AtomDescr>* >   bmpingAtoms;
 	std::vector<std::list<AtomDescr > > bumpingNonBonded( numItems );

commit 9dd1883d4b756a43092ab920564abd78a1d59505

Fixing a bug introduced in commit f5b3046be9c20d82be2974bdf457a9c68d6a808e regarding filling in a vector, switched to resize() and [] rather than reserve() and push_back() because not all entries are filled in.

reduce_src/reduce.cpp
@@ -1879,7 +1879,7 @@ void genHydrogens(const atomPlacementPlan& pp, ResBlk& theRes, bool o2prime,
 			int maxalt = 0;
 			std::vector<int> nconf;
 			std::vector<char> all_confs;
-			nconf.reserve(numConnAtoms);
+			nconf.resize(numConnAtoms);
 
 			std::vector< std::vector<PDBrec*> > rvv;
 			rvv.reserve(numConnAtoms);
@@ -1889,7 +1889,7 @@ void genHydrogens(const atomPlacementPlan& pp, ResBlk& theRes, bool o2prime,
 				std::list<PDBrec*> rs;
 				theRes.get(pp.conn(i), rs);
 				if (!rs.empty()) {
-					nconf.push_back(rs.size());
+					nconf[i] = rs.size();
 					maxalt = std::max(maxalt, nconf[i]);
 					std::vector<PDBrec*> rvv_v;
 					rvv_v.reserve(nconf[i]);