17d844aabf22b34743698f88461ba75f3b9ffc89
Russ Taylor <russ@reliasolve.com>
2020-12-11 15:21:31 -0500

    Adding Python script to use the dynamic library to run the program.  Does not yet fully implement the behavior.

---
+++ reduce_src/reduce.py
@@ -0,0 +1,37 @@
+import sys
+import reduce
+
+# @todo Add a main function and parse the command line
+# @todo Make Python 2 and 3 compatible
+
+fileName = sys.argv[1]
+
+with open(fileName) as f:
+  input = f.read()
+
+models = reduce.inputModels(input)
+print("Found "+str(len(models))+" models:")
+for m in models:
+  print(" Model size "+str(m.size()))
+  if reduce.getRemoveATOMHydrogens() or reduce.getRemoveOtherHydrogens():
+    reduce.dropHydrogens(m, reduce.getRemoveATOMHydrogens(), reduce.getRemoveOtherHydrogens())
+
+  UseSEGIDasChain = reduce.checkSEGIDs(m)
+  print(" UseSEGIDasChain = "+str(UseSEGIDasChain))
+
+  # @todo We need to get this working in a way that it can be copied for Python
+  # @todo Point this at the general location.
+  # hetdatabase = reduce.CTab("/usr/local/reduce_wwPDB_het_dict.txt")
+
+  dotBucket = reduce.DotSphManager(reduce.getVdwDotDensity())
+
+  xyz = reduce.AtomPositions(2000, reduce.getDoOnlyAltA(), reduce.getUseXplorNames(),
+          reduce.getUseOldNames(), reduce.getBackBoneModel(),
+          reduce.getNBondCutoff(), reduce.getMinRegHBgap(),
+          reduce.getMinChargedHBgap(),
+          reduce.getBadBumpGapCut(), dotBucket,
+          reduce.getProbeRadius(),
+          reduce.getPenaltyMagnitude(), reduce.getOccupancyCutoff(),
+          reduce.getVerbose(), reduce.getShowOrientScore(),
+          reduce.getShowCliqueTicks()
+        )
