b41c4d49fe5c46152d064c125467bbfd765de4a5
Russ Taylor <russ@reliasolve.com>
2020-12-17 15:08:51 -0500

    Initial attempt to move to CCTBX-aware Python wrapping

---
+++ reduce_src/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import, division, print_function
+# -*- coding: utf-8 -*-
+import boost_adaptbx.boost.python as bp
+ext = bp.import_ext("cctbx_reduce_ext")
+from cctbx_reduce_ext import *
+
