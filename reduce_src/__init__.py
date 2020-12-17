33c4cfa4b5788dc05a87132f2b0fe518b7cc553a
Russ Taylor <russ@reliasolve.com>
2020-12-17 15:54:49 -0500

    Seeing if we need the __init__.py file

--- reduce_src/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import, division, print_function
-# -*- coding: utf-8 -*-
-import boost_adaptbx.boost.python as bp
-ext = bp.import_ext("cctbx_reduce_ext")
-from cctbx_reduce_ext import *
-
